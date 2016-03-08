3f415898124a0c34cf969274865bd357c8776e50
丁健 <798811246@qq.com>
2016-03-08 22:22:16 +0800

    0308
    0308

---
+++ YunTu/ViewController/My/YTMyViewController.m
@@ -0,0 +1,42 @@
+//
+//  YTMyViewController.m
+//  YunTu
+//
+//  Created by 丁健 on 16/3/8.
+//  Copyright © 2016年 丁健. All rights reserved.
+//
+
+#import "YTMyViewController.h"
+
+@interface YTMyViewController ()
+
+@end
+
+@implementation YTMyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self createBaseView];
+}
+
+- (void)createBaseView
+{
+    self.title = @"我";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ YunTu/ViewController/YTLearn/YTLearnViewController.h
@@ -0,0 +1,13 @@
+//
+//  YTLearnViewController.h
+//  YunTu
+//
+//  Created by 丁健 on 16/3/8.
+//  Copyright © 2016年 丁健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+@interface YTLearnViewController : BaseViewController
+
+@end
